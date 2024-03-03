b0a1a60a9b95b9682da8c69b84bfe41b4abc475b
Paschalis <pasxalis.9@hotmail.com>
2024-03-03 16:32:23 +0200

    Modified Clock_Dimmer.ino

--- Clock_Dimmer.ino
+++ Clock_Dimmer.ino
@@ -5,210 +5,6 @@
    Pin 12 becomes high when alarm occurred and button B2 returns it to low and
    turns the occurred alarm OFF.
    DS3231 interrupt pin is connected to Arduino external interrupt pin 2.
-
-
-   
-############################ The code below uses the timer function rather than a delay ###############################
-
-AC Light Control
- 
- Updated by Robert Twomey 
- 
- Changed zero-crossing detection to look for RISING edge rather
- than falling.  (originally it was only chopping the negative half
- of the AC wave form). 
- 
- Also changed the dim_check() to turn on the Triac, leaving it on 
- until the zero_cross_detect() turn's it off.
- 
- Adapted from sketch by Ryan McLaughlin 
- http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1230333861/30
- 
- 
-
-#include  <TimerOne.h>          // Avaiable from http://www.arduino.cc/playground/Code/Timer1
-volatile int i=0;               // Variable to use as a counter volatile as it is in an interrupt
-volatile boolean zero_cross=0;  // Boolean to store a "switch" to tell us if we have crossed zero
-int AC_pin = 11;                // Output to Opto Triac
-int dim = 0;                    // Dimming level (0-128)  0 = on, 128 = 0ff
-int inc=1;                      // counting up or down, 1=up, -1=down
-
-int freqStep = 75;    // This is the delay-per-brightness step in microseconds.
-                      // For 60 Hz it should be 65
-// It is calculated based on the frequency of your voltage supply (50Hz or 60Hz)
-// and the number of brightness steps you want. 
-// 
-// Realize that there are 2 zerocrossing per cycle. This means
-// zero crossing happens at 120Hz for a 60Hz supply or 100Hz for a 50Hz supply. 
-
-// To calculate freqStep divide the length of one full half-wave of the power
-// cycle (in microseconds) by the number of brightness steps. 
-//
-// (120 Hz=8333uS) / 128 brightness steps = 65 uS / brightness step
-// (100Hz=10000uS) / 128 steps = 75uS/step
-
-void setup() {                                      // Begin setup
-  pinMode(AC_pin, OUTPUT);                          // Set the Triac pin as output
-  attachInterrupt(0, zero_cross_detect, RISING);    // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
-  Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
-  Timer1.attachInterrupt(dim_check, freqStep);      
-  // Use the TimerOne Library to attach an interrupt
-  // to the function we use to check to see if it is 
-  // the right time to fire the triac.  This function 
-  // will now run every freqStep in microseconds.                                            
-}
-
-void zero_cross_detect() {    
-  zero_cross = true;               // set the boolean to true to tell our dimming function that a zero cross has occured
-  i=0;
-  digitalWrite(AC_pin, LOW);       // turn off TRIAC (and AC)
-}                                 
-
-// Turn on the TRIAC at the appropriate time
-void dim_check() {                   
-  if(zero_cross == true) {              
-    if(i>=dim) {                     
-      digitalWrite(AC_pin, HIGH); // turn on light       
-      i=0;  // reset time step counter                         
-      zero_cross = false; //reset zero cross detection
-    } 
-    else {
-      i++; // increment time step counter                     
-    }                                
-  }                                  
-}                                   
-
-void loop() {                        
-  dim+=inc;
-  if((dim>=128) || (dim<=0))
-    inc*=-1;
-  delay(18);
-}
-
-
-
-################################### AC Voltage dimmer with Zero cross detection #######################################
-
-
-AC Voltage dimmer with Zero cross detection
-Author: Charith Fernanado Adapted by DIY_bloke
-License: Creative Commons Attribution Share-Alike 3.0 License.
-Attach the Zero cross pin of the module to Arduino External Interrupt pin
-Select the correct Interrupt # from the below table 
-(the Pin numbers are digital pins, NOT physical pins: 
-digital pin 2 [INT0]=physical pin 4 and digital pin 3 [INT1]= physical pin 5)
-check: <a href="http://arduino.cc/en/Reference/attachInterrupt">interrupts</a>
-
-Pin    |  Interrrupt # | Arduino Platform
----------------------------------------
-2      |  0            |  All -But it is INT1 on the Leonardo
-3      |  1            |  All -But it is INT0 on the Leonardo
-18     |  5            |  Arduino Mega Only
-19     |  4            |  Arduino Mega Only
-20     |  3            |  Arduino Mega Only
-21     |  2            |  Arduino Mega Only
-0      |  0            |  Leonardo
-1      |  3            |  Leonardo
-7      |  4            |  Leonardo
-The Arduino Due has no standard interrupt pins as an iterrupt can be attached to almosty any pin. 
-
-In the program pin 2 is chosen
-
-
-int AC_LOAD = 3;    // Output to Opto Triac pin
-int dimming = 128;  // Dimming level (0-128)  0 = ON, 128 = OFF
-
-void setup()
-{
-  pinMode(AC_LOAD, OUTPUT);// Set AC Load pin as output
-  attachInterrupt(0, zero_crosss_int, RISING);  // Choose the zero cross interrupt # from the table above
-}
-
-//the interrupt function must take no parameters and return nothing
-void zero_crosss_int()  //function to be fired at the zero crossing to dim the light
-{
-  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms 
-  // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle) 
-  // For 60Hz => 8.33ms (10.000/120)
-  // 10ms=10000us
-  // (10000us - 10us) / 128 = 75 (Approx) For 60Hz =>65
-
-  int dimtime = (75*dimming);    // For 60Hz =>65    
-  delayMicroseconds(dimtime);    // Wait till firing the TRIAC    
-  digitalWrite(AC_LOAD, HIGH);   // Fire the TRIAC
-  delayMicroseconds(10);         // triac On propogation delay 
-         // (for 60Hz use 8.33) Some Triacs need a longer period
-  digitalWrite(AC_LOAD, LOW);    // No longer trigger the TRIAC (the next zero crossing will swith it off) TRIAC
-}
-
-void loop()  {
-  for (int i=5; i <= 128; i++){
-    dimming=i;
-    delay(10);
-   }
-}
-
-
-
-
-
-############################################## DHT11 ###############################################################
-
-// Interfacing Arduino with DHT11 humidity and temperature sensor
- 
-// include LCD library code
-#include <LiquidCrystal.h>
-// include DHT library code
-#include "DHT.h"
- 
-#define DHTPIN 13            // DHT11 data pin is connected to Arduino pin 13
- 
-// LCD module connections (RS, E, D4, D5, D6, D7)
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
- 
-#define DHTTYPE DHT11       // DHT11 sensor is used
-DHT dht(DHTPIN, DHTTYPE);   // Initialize DHT library
- 
-char temperature[] = "Temp = 00.0 C  ";
-char humidity[]    = "RH   = 00.0 %  ";
-void setup() {
-  // set up the LCD's number of columns and rows
-  lcd.begin(16, 2);
-  dht.begin();
-}
- 
-void loop() {
-  delay(1000);           // wait 1s between readings
-  // Read humidity
-  byte RH = dht.readHumidity();
-  //Read temperature in degree Celsius
-  byte Temp = dht.readTemperature();
-  
-  // Check if any reads failed and exit early (to try again)
-  if (isnan(RH) || isnan(Temp)) {
-    lcd.clear();
-    lcd.setCursor(5, 0);
-    lcd.print("Error");
-    return;
-  }
- 
-  temperature[7]     = Temp / 10 + 48;
-  temperature[8]     = Temp % 10 + 48;
-  temperature[11]    = 223;
-  humidity[7]        = RH / 10 + 48;
-  humidity[8]        = RH % 10 + 48;
-  lcd.setCursor(0, 0);
-  lcd.print(temperature);
-  lcd.setCursor(0, 1);
-  lcd.print(humidity);
-}
-
-####################################################################################################################
-
-
-
-
-
 */
 
 // include LCD library code
@@ -528,3 +324,211 @@ void loop() {
   DS3231_display();                             // Display time & calendar
   delay(50);                                    // Wait 50ms
 }
+
+
+/*
+ *
+ *
+ *
+
+
+############################ The code below uses the timer function rather than a delay ###############################
+
+AC Light Control
+
+ Updated by Robert Twomey
+
+ Changed zero-crossing detection to look for RISING edge rather
+ than falling.  (originally it was only chopping the negative half
+ of the AC wave form).
+
+ Also changed the dim_check() to turn on the Triac, leaving it on
+ until the zero_cross_detect() turn's it off.
+
+ Adapted from sketch by Ryan McLaughlin
+ http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1230333861/30
+
+
+
+#include  <TimerOne.h>          // Avaiable from http://www.arduino.cc/playground/Code/Timer1
+volatile int i=0;               // Variable to use as a counter volatile as it is in an interrupt
+volatile boolean zero_cross=0;  // Boolean to store a "switch" to tell us if we have crossed zero
+int AC_pin = 11;                // Output to Opto Triac
+int dim = 0;                    // Dimming level (0-128)  0 = on, 128 = 0ff
+int inc=1;                      // counting up or down, 1=up, -1=down
+
+int freqStep = 75;    // This is the delay-per-brightness step in microseconds.
+                      // For 60 Hz it should be 65
+// It is calculated based on the frequency of your voltage supply (50Hz or 60Hz)
+// and the number of brightness steps you want.
+//
+// Realize that there are 2 zerocrossing per cycle. This means
+// zero crossing happens at 120Hz for a 60Hz supply or 100Hz for a 50Hz supply.
+
+// To calculate freqStep divide the length of one full half-wave of the power
+// cycle (in microseconds) by the number of brightness steps.
+//
+// (120 Hz=8333uS) / 128 brightness steps = 65 uS / brightness step
+// (100Hz=10000uS) / 128 steps = 75uS/step
+
+void setup() {                                      // Begin setup
+  pinMode(AC_pin, OUTPUT);                          // Set the Triac pin as output
+  attachInterrupt(0, zero_cross_detect, RISING);    // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
+  Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
+  Timer1.attachInterrupt(dim_check, freqStep);
+  // Use the TimerOne Library to attach an interrupt
+  // to the function we use to check to see if it is
+  // the right time to fire the triac.  This function
+  // will now run every freqStep in microseconds.
+}
+
+void zero_cross_detect() {
+  zero_cross = true;               // set the boolean to true to tell our dimming function that a zero cross has occured
+  i=0;
+  digitalWrite(AC_pin, LOW);       // turn off TRIAC (and AC)
+}
+
+// Turn on the TRIAC at the appropriate time
+void dim_check() {
+  if(zero_cross == true) {
+    if(i>=dim) {
+      digitalWrite(AC_pin, HIGH); // turn on light
+      i=0;  // reset time step counter
+      zero_cross = false; //reset zero cross detection
+    }
+    else {
+      i++; // increment time step counter
+    }
+  }
+}
+
+void loop() {
+  dim+=inc;
+  if((dim>=128) || (dim<=0))
+    inc*=-1;
+  delay(18);
+}
+
+
+
+################################### AC Voltage dimmer with Zero cross detection #######################################
+
+
+AC Voltage dimmer with Zero cross detection
+Author: Charith Fernanado Adapted by DIY_bloke
+License: Creative Commons Attribution Share-Alike 3.0 License.
+Attach the Zero cross pin of the module to Arduino External Interrupt pin
+Select the correct Interrupt # from the below table
+(the Pin numbers are digital pins, NOT physical pins:
+digital pin 2 [INT0]=physical pin 4 and digital pin 3 [INT1]= physical pin 5)
+check: <a href="http://arduino.cc/en/Reference/attachInterrupt">interrupts</a>
+
+Pin    |  Interrrupt # | Arduino Platform
+---------------------------------------
+2      |  0            |  All -But it is INT1 on the Leonardo
+3      |  1            |  All -But it is INT0 on the Leonardo
+18     |  5            |  Arduino Mega Only
+19     |  4            |  Arduino Mega Only
+20     |  3            |  Arduino Mega Only
+21     |  2            |  Arduino Mega Only
+0      |  0            |  Leonardo
+1      |  3            |  Leonardo
+7      |  4            |  Leonardo
+The Arduino Due has no standard interrupt pins as an iterrupt can be attached to almosty any pin.
+
+In the program pin 2 is chosen
+
+
+int AC_LOAD = 3;    // Output to Opto Triac pin
+int dimming = 128;  // Dimming level (0-128)  0 = ON, 128 = OFF
+
+void setup()
+{
+  pinMode(AC_LOAD, OUTPUT);// Set AC Load pin as output
+  attachInterrupt(0, zero_crosss_int, RISING);  // Choose the zero cross interrupt # from the table above
+}
+
+//the interrupt function must take no parameters and return nothing
+void zero_crosss_int()  //function to be fired at the zero crossing to dim the light
+{
+  // Firing angle calculation : 1 full 50Hz wave =1/50=20ms
+  // Every zerocrossing thus: (50Hz)-> 10ms (1/2 Cycle)
+  // For 60Hz => 8.33ms (10.000/120)
+  // 10ms=10000us
+  // (10000us - 10us) / 128 = 75 (Approx) For 60Hz =>65
+
+  int dimtime = (75*dimming);    // For 60Hz =>65
+  delayMicroseconds(dimtime);    // Wait till firing the TRIAC
+  digitalWrite(AC_LOAD, HIGH);   // Fire the TRIAC
+  delayMicroseconds(10);         // triac On propogation delay
+         // (for 60Hz use 8.33) Some Triacs need a longer period
+  digitalWrite(AC_LOAD, LOW);    // No longer trigger the TRIAC (the next zero crossing will swith it off) TRIAC
+}
+
+void loop()  {
+  for (int i=5; i <= 128; i++){
+    dimming=i;
+    delay(10);
+   }
+}
+
+
+
+
+
+############################################## DHT11 ###############################################################
+
+// Interfacing Arduino with DHT11 humidity and temperature sensor
+
+// include LCD library code
+#include <LiquidCrystal.h>
+// include DHT library code
+#include "DHT.h"
+
+#define DHTPIN 13            // DHT11 data pin is connected to Arduino pin 13
+
+// LCD module connections (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+#define DHTTYPE DHT11       // DHT11 sensor is used
+DHT dht(DHTPIN, DHTTYPE);   // Initialize DHT library
+
+char temperature[] = "Temp = 00.0 C  ";
+char humidity[]    = "RH   = 00.0 %  ";
+void setup() {
+  // set up the LCD's number of columns and rows
+  lcd.begin(16, 2);
+  dht.begin();
+}
+
+void loop() {
+  delay(1000);           // wait 1s between readings
+  // Read humidity
+  byte RH = dht.readHumidity();
+  //Read temperature in degree Celsius
+  byte Temp = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again)
+  if (isnan(RH) || isnan(Temp)) {
+    lcd.clear();
+    lcd.setCursor(5, 0);
+    lcd.print("Error");
+    return;
+  }
+
+  temperature[7]     = Temp / 10 + 48;
+  temperature[8]     = Temp % 10 + 48;
+  temperature[11]    = 223;
+  humidity[7]        = RH / 10 + 48;
+  humidity[8]        = RH % 10 + 48;
+  lcd.setCursor(0, 0);
+  lcd.print(temperature);
+  lcd.setCursor(0, 1);
+  lcd.print(humidity);
+}
+
+####################################################################################################################
+
+
+
+ */
